1581447b2ad4183d00a0eff295080129e9f41b93
Jackson Barreto <jackson.barreto@gmail.com>
2021-06-05 23:08:12 +0100

    corrigido notificações

--- src/app/notificacoes/notificacoes.page.ts
+++ src/app/notificacoes/notificacoes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { ModalController, NavController } from '@ionic/angular';
 
 @Component({
@@ -8,7 +9,7 @@ import { ModalController, NavController } from '@ionic/angular';
 })
 export class NotificacoesPage {
 
-  constructor(public modalController: ModalController, public navRoot: NavController) { }
+  constructor(public modalController: ModalController, public navRoot: NavController, private router: Router) { }
 
   dismiss() {
     this.modalController.dismiss({
@@ -17,12 +18,67 @@ export class NotificacoesPage {
   }
 
   onClick() {
-    this.navRoot.navigateForward('detalhe-momento');
+    let navigationExtras: NavigationExtras;
+    //this.navRoot.navigateForward('detalhe-momento');
+    navigationExtras = {
+      state: {
+        albumName: "Jackson Barreto",
+        imageSrc: "../../assets/img/albuns/f01.jpg",
+        time: "12:22",
+        fotos: null,
+        titulo: "Churrascada",
+        data: "2021-07-18",
+        descricao: "Confraternização de fim de semestre.",
+        detalhes: "12MP 1440x720 22.4MB",
+        tags: "Festa,Amigos"
+      }
+    };
+    this.router.navigate(['detalhe-momento'], navigationExtras);
     this.dismiss();
   }
 
   onClick2() {
-    this.navRoot.navigateForward('momentos');
+    let navigationExtras: NavigationExtras;
+    //this.navRoot.navigateForward('detalhe-momento');
+    navigationExtras = {
+      state: {
+        albumName: "Jackson Barreto",
+        imageSrc: "../../assets/img/albuns/f01.jpg",
+        time: "12:22",
+        fotos: JSON.stringify([
+          {
+            "titulo": "a",
+            "hora": "07:54",
+            "data": "2021-06-15",
+            "descricao": "Festa animada.",
+            "detalhes": "0.8MP 1440x720 3.4MB",
+            "tags": [
+              "festa",
+              "DCB"
+            ],
+            "src": "../../assets/img/albuns/f01.jpg"
+          },
+          {
+            "titulo": "b",
+            "hora": "07:54",
+            "data": "2021-02-31",
+            "descricao": "Festa animada.",
+            "detalhes": "0.8MP 1440x720 3.4MB",
+            "tags": [
+              "festa",
+              "DCB"
+            ],
+            "src": "../../assets/img/albuns/f02.jpg"
+          }
+        ]),
+        titulo: "Churrascada",
+        data: "2021-07-18",
+        descricao: "Confraternização de fim de semestre.",
+        detalhes: "12MP 1440x720 22.4MB",
+        tags: "Festa,Amigos"
+      }
+    };
+    this.router.navigate(['momentos'], navigationExtras);
     this.dismiss();
   }
 
