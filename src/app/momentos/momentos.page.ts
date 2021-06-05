94c4bca3efee601298dd2552070c76cd2c0a574e
Jackson Barreto <jackson.barreto@gmail.com>
2021-06-06 00:38:09 +0100

    video funcional

--- src/app/momentos/momentos.page.ts
+++ src/app/momentos/momentos.page.ts
@@ -39,7 +39,7 @@ export class MomentosPage implements OnInit {
       this.sliceEnd = this.router.getCurrentNavigation().extras.state.sliceEnd;
       this.albuns = JSON.parse(this.router.getCurrentNavigation().extras.state.fotos);
     }
-
+    console.log(this.albuns)
   }
 
   ngOnInit() {
@@ -145,7 +145,6 @@ export class MomentosPage implements OnInit {
   public getNavRoot(): NavController {
     return this.navRoot;
   }
-
 
 
 }
